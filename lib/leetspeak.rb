e6e533bc89affb0855fc00eab5a04ce983c24eb0
Student <example@example.com>
2019-07-15 14:49:18 -0700

    Dry my code

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,9 +1,7 @@
-userinput = gets.chomp
-p userinput
-def leetspeak (userinput)
-  userinput =  userinput.tr('e', '3').tr('o', '0').tr('I', '1')
-  arr = userinput.split
-  our_array = arr.map{|x| x[0] + x[1..-1].tr('s', 'z')}
-  phrase = our_array.join(' ')
+class String
+def leetspeak
+  split.map{|x| x[0] + x[1..-1].tr('s', 'z')}.join(' ').tr('eEoOI', '33001')
+  end
 end
-p leetspeak(userinput)
+p 'What would you like to leetspeak?'
+p gets.leetspeak
